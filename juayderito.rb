a53ed250781141e493156773d5073ea929f363c4
Oscar Gonzalez <ogonzalez@tacitknowledge.com>
2011-02-18 21:00:10 -0500

    juay b.01

--- juayderito.rb
+++ juayderito.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'twitter'
 require 'haml'
 get '/' do
-    search = Twitter::Search.new
-    @juay = "#{search.hashtag("juay").no_retweets.per_page(1).page(rand(100)).fetch.first.text}"
+    juays =  ["juay", "juayderito"]
+    juay_term = juays[rand(juays.length)]
+    search = Twitter::Search.new.hashtag(juay_term).no_retweets.per_page(1).page(rand(100)).fetch.first
+    @ju = '@' + search.from_user
+    @juay = search.text
+    
     haml :index
 end
