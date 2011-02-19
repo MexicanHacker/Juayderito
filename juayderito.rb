7c47f5bc13c23708dcdff1da61cf54ef1d07b220
Daniel Valencia <danvalencia@gmail.com>
2011-02-19 16:35:09 -0600

    ecstracted clazz. +new_tweet action

--- juayderito.rb
+++ juayderito.rb
@@ -1,12 +1,39 @@
+require 'rubygems'
 require 'sinatra'
 require 'twitter'
 require 'haml'
-get '/' do
-    juays =  ["juay", "juayderito"]
-    juay_term = juays[rand(juays.length)]
-    search = Twitter::Search.new.hashtag(juay_term).no_retweets.per_page(1).page(rand(100)).fetch.first
-    @ju = '@' + search.from_user
-    @juay = search.text
+
+get '/' do    
+    tweet = TweetFetcher.get_tweet do |t|
+      t.hashes = ["juay", "juayderito"]
+    end
+    
+    @ju = '@' + tweet.from_user
+    @juay = tweet.text
     
     haml :index
+end
+
+get '/new_tweet' do
+    tweet = TweetFetcher.get_tweet do |t|
+      t.hashes = ["juay", "juayderito"]
+    end
+    
+    {:name => tweet.from_user, :text => tweet.text}.to_json
+end
+
+
+class TweetFetcher
+  attr_accessor :hashes
+  
+  def self.get_tweet
+    t = TweetFetcher.new
+    yield t
+    hash_term = t.get_term 
+    Twitter::Search.new.hashtag(hash_term).no_retweets.per_page(1).page(rand(100)).fetch.first
+  end
+    
+  def get_term
+    @hashes[rand(@hashes.length)]
+  end
 end
